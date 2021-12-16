f615884173c03d135ebea55fb3a9b6abd4772052
lydenchai <lyeden999@gmail.com>
2021-12-16 07:35:52 +0700

    Vue sample cv 2

---
+++ src/components/views/FormsInputsView.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-row class="pa-6">
+    <v-col md="12">
+      <v-alert dense :type="alertType">
+        I'm a dense alert with a <strong>{{ alertType }}</strong> of info
+      </v-alert>
+
+      <v-row>
+        <v-col md="12">
+          <v-file-input truncate-length="15"></v-file-input>
+        </v-col>
+        <v-col md="6">
+          <v-combobox
+            v-model="select"
+            :items="items"
+            label="Combobox"
+            multiple
+            outlined
+            dense
+          ></v-combobox>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      select: ["Vuetify", "Programming"],
+      items: ["Programming", "Design", "Vue", "Vuetify"],
+      alertType: "success",
+    };
+  },
+};
+</script>
+ 
